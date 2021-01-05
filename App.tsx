edcd32e389a4245e0df2b1d0c7b8b37ff57b19af
Miroslav Frodl <miroslav.frodl@icewarp.com>
2021-01-05 08:42:47 +0100

    init

---
+++ App.tsx
@@ -0,0 +1,90 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ */
+
+import React from "react";
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View } from "react-native";
+import { DebugInstructions, Header, LearnMoreLinks, ReloadInstructions } from "react-native/Libraries/NewAppScreen";
+
+const App = (): JSX.Element => {
+	return (
+		<>
+			<StatusBar barStyle="dark-content" />
+			<SafeAreaView>
+				<ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
+					<Header />
+					<View style={styles.body}>
+						<View style={styles.sectionContainer}>
+							<Text style={styles.sectionTitle}>Step One</Text>
+							<Text style={styles.sectionDescription}>
+								Edit <Text style={styles.highlight}>App.tsx</Text> to change this screen and then come back to see your edits.
+							</Text>
+						</View>
+						<View style={styles.sectionContainer}>
+							<Text style={styles.sectionTitle}>See Your Changes</Text>
+							<Text style={styles.sectionDescription}>
+								<ReloadInstructions />
+							</Text>
+						</View>
+						<View style={styles.sectionContainer}>
+							<Text style={styles.sectionTitle}>Debug</Text>
+							<Text style={styles.sectionDescription}>
+								<DebugInstructions />
+							</Text>
+						</View>
+						<View style={styles.sectionContainer}>
+							<Text style={styles.sectionTitle}>Learn More</Text>
+							<Text style={styles.sectionDescription}>Read the docs to discover what to do next:</Text>
+						</View>
+						<LearnMoreLinks />
+					</View>
+				</ScrollView>
+			</SafeAreaView>
+		</>
+	);
+};
+
+const styles = StyleSheet.create({
+	body: {
+		backgroundColor: "white"
+	},
+	engine: {
+		position: "absolute",
+		right: 0
+	},
+	footer: {
+		color: "black",
+		fontSize: 12,
+		fontWeight: "600",
+		padding: 4,
+		paddingRight: 12,
+		textAlign: "right"
+	},
+	highlight: {
+		fontWeight: "700"
+	},
+	scrollView: {
+		backgroundColor: "white"
+	},
+	sectionContainer: {
+		marginTop: 32,
+		paddingHorizontal: 24
+	},
+	sectionDescription: {
+		fontSize: 18,
+		fontWeight: "400",
+		marginTop: 8
+	},
+	sectionTitle: {
+		color: "blue",
+		fontSize: 24,
+		fontWeight: "600"
+	}
+});
+
+export default App;
